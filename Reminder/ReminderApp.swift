58ddedc0df8b20bb8106923c819029b980648d4e
Nur Hidayat <hidayat.speed78@gmai.com>
2021-07-05 09:31:54 +0700

    Initial Commit

---
+++ Reminder/ReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReminderApp.swift
+//  Reminder
+//
+//  Created by User on 05/07/21.
+//
+
+import SwiftUI
+
+@main
+struct ReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
